b83065a8c92a55a6e040c2ccd87dedc7e0b18ffc
Hannah Hughes <hugheshannah96@gmail.com>
2018-05-29 23:03:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, obj)
-  delete newObj.key
+  const newObj = Object.assign({}, object)
+  delete newObj[key]
   return newObj
 }
